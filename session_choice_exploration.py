195b9095c03cdc6b06dd16fc641cdd5da4307de5
Sasha Golubeva <sgolubev@login2.talapas.uoregon.edu>
2023-11-11 17:07:38 -0800

    plotting three graphs on subplots

--- session_choice_exploration.py
+++ session_choice_exploration.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+#The purpose of this script is to compare glm-hmm performance on small to large amount of data points 
+# To acomplish this, we create a standard glm-hmm model initialized with preset transition matrix and 
+# weights. Then we generate syntetic data for different numbers of sessions and trials and initialize
+# and fit a new glm-hmm model. Then we compare weights and biases from the model initialized with
+# known parameters vs weights and biases recovered from each new model and plot them. Additionally,
+# we plot states determined by the model and choices to investigate how exactly model classifies states
+# based on choices. 
+
 import numpy as np
 import numpy.random as npr
 import matplotlib.pyplot as plt
@@ -32,20 +41,21 @@ def make_standard_hmm(gen_weights, gen_log_trans_mat ):
 
 def generate_inputs(num_trials_per_sess):
 
-    """Takes """
+    """Takes number of trials per session generates inputs from stimulus values and choices returns
+    inputs"""
 
-    #num_sess = sess  # number of example sessions (set to 10)
-    #num_trials_per_sess = j  # number of trials in a session
+
     print(f'generate ipts{num_trials_per_sess=}')
     inpts = np.ones((num_sess, num_trials_per_sess, input_dim))  # initialize inpts array
-    #stim_vals = [-1, -0.5, -0.25, -0.125, -0.0625, 0, 0.0625, 0.125, 0.25, 0.5, 1]
+    stim_vals = [-1, -0.5, -0.25, -0.125, -0.0625, 0, 0.0625, 0.125, 0.25, 0.5, 1]
     inpts[:, :, 0] = np.random.choice(stim_vals, (num_sess, num_trials_per_sess))  # generate random sequence of stimuli
     inpts = list(inpts)  # convert inpts to correct format
     return inpts
 
 def true_ll_model(num_trials_per_sess, num_sess):
 
-    """"Takes""" 
+    """"Takes number of trials per session and number of sessions returns log likelihood for the model
+    initialized with standard weights and transition matrix""" 
     true_latents, true_choices = [], []
     for sess in range(num_sess):
         true_z, true_y = true_glmhmm.sample(num_trials_per_sess, input=inpts[sess])
@@ -57,67 +67,70 @@ def true_ll_model(num_trials_per_sess, num_sess):
 
 def fit_glm_hmm(new_glmhmm):
 
-    """Takes"""
+    """Takes newly initialized hmm glm model (without standard transition matrix and weights) and 
+    returns a fitted glm hmm"""
 
     fit_ll = new_glmhmm.fit(true_choices, inputs=inpts, method="em", num_iters=N_iters, tolerance=10**-4)
 
     return fit_ll
 
 
-def plot_weights(new_glmhmm):
+def plot_weights(new_glmhmm, ax):
 
-    """Takes """
+    """Takes fitted glm hmm and axis object for plotting and plots standard and recovered weights 
+    on the first of three subplots"""
 
-    fig = plt.figure(figsize=(12, 6), dpi=80, facecolor='w', edgecolor='k')
+    #fig = plt.figure(figsize=(12, 6), dpi=80, facecolor='w', edgecolor='k')
     cols = ['#ff7f00', '#4daf4a', '#377eb8']
     recovered_weights = new_glmhmm.observations.params
     #print(recovered_weights)
     for k in range(num_states):
         if k == 0:
-            plt.plot(range(input_dim), gen_weights[k][0], marker='o',
+            ax.plot(range(input_dim), gen_weights[k][0], marker='o',
                     color=cols[k], linestyle='-',
                     lw=1.5, label="generative")
-            plt.plot(range(input_dim), recovered_weights[k][0], color=cols[k],
+            ax.plot(range(input_dim), recovered_weights[k][0], color=cols[k],
                         lw=1.5,  label="recovered", linestyle='--')
         else:
-            plt.plot(range(input_dim), gen_weights[k][0], marker='o',
+            ax.plot(range(input_dim), gen_weights[k][0], marker='o',
                     color=cols[k], linestyle='-',
                     lw=1.5, label="")
-            plt.plot(range(input_dim), recovered_weights[k][0], color=cols[k],
+            ax.plot(range(input_dim), recovered_weights[k][0], color=cols[k],
                         lw=1.5, label="", linestyle='--')
-    plt.yticks(fontsize=10)
-    plt.ylabel("GLM weight", fontsize=15)
-    plt.xlabel("covariate", fontsize=15)
-    plt.xticks([0, 1], ['stimulus', 'bias'], fontsize=12, rotation=45)
-    plt.axhline(y=0, color="k", alpha=0.5, ls="--")
-    plt.legend()
-    plt.title(f"Weight recovery (Set {j})", fontsize=15)
+    #ax.set_yticks(fontsize=10)
+    ax.set_ylabel("GLM weight", fontsize=25)
+    ax.set_xlabel("covariate", fontsize=25)
+    ax.set_xticks([0, 1], ['stimulus', 'bias'], fontsize=25, rotation=25)
+    ax.axhline(y=0, color="k", alpha=0.5, ls="--")
+    ax.legend()
+    ax.set_title(f"Weight recovery (Set {j})", fontsize=25)
     #plt.savefig(f'{out}_{j}.png')
-    return fig
+    #return fig
 
-def plot_states(new_glmhmm):
+def plot_states(new_glmhmm, ax):
 
-    """Takes"""
+    """Takes fitted glm hmm and an axis object for plotting and plots states on the second subplot"""
 
     cols = ['#ff7f00', '#4daf4a', '#377eb8']
     posterior_probs = [new_glmhmm.expected_states(data=data, input=inpt)[0]
                 for data, inpt
                 in zip(true_choices, inpts)]
-    fig = plt.figure(figsize=(15, 2.5), dpi=80, facecolor='w', edgecolor='k')
+    #fig = plt.figure(figsize=(15, 2.5), dpi=80, facecolor='w', edgecolor='k')
     sess_id = 0 #session id; can choose any index between 0 and num_sess-1
     for k in range(num_states):
-        plt.plot(posterior_probs[sess_id][:, k], label="State " + str(k + 1), lw=2,
+        ax.plot(posterior_probs[sess_id][:, k], label="State " + str(k + 1), lw=2,
                 color=cols[k])
-    plt.ylim((-0.01, 1.01))
-    plt.yticks([0, 0.5, 1], fontsize = 10)
-    plt.xlabel("trial #", fontsize = 15)
-    plt.ylabel("p(state)", fontsize = 15)
+    ax.set_ylim((-0.01, 1.01))
+    ax.set_yticks([0, 0.5, 1]) # had to remove  fontsize = 10 because mpl complained 
+    ax.set_xlabel("trial #", fontsize = 25)
+    ax.set_ylabel("p(state)", fontsize = 25)
+    ax.set_title(f"States (Set {j})", fontsize=25)
     #plt.savefig(f'states_fig{out}session{j}.png')
-    return fig
+    #return fig
 
-def plot_choices(n_sess):
+def plot_choices(n_sess, ax):
 
-    """Takes"""
+    """Takes fitted glm hmm and axis object for plotting and plots choices on the third subplot"""
 
     ins = inpts[0] # accessing 0th element in the outer list 
     ins = ins[0:, 0] # accessing entire column in the inputs 0th colunm, all rows
@@ -133,29 +146,30 @@ def plot_choices(n_sess):
     alpha = 0.6
     jitter = 0.1 * np.random.randn(len(cho))
     y_values_jittered = jitter+cho
-    fig = plt.figure(figsize=(15, 2.5), dpi=80, facecolor='w', edgecolor='k')
-    ax = plt.axes()
+    #fig = plt.figure(figsize=(15, 2.5), dpi=80, facecolor='w', edgecolor='k')
+    #ax = plt.axes()
     x_range = np.array(range(len(cho)))
-    scatter_correct = plt.scatter(x_range[correct_choices], y_values_jittered[correct_choices], 
+    scatter_correct = ax.scatter(x_range[correct_choices], y_values_jittered[correct_choices], 
                                   label='correct', color='r', alpha=alpha)
-    scatter_wrong = plt.scatter(x_range[~correct_choices], y_values_jittered[~correct_choices],
+    scatter_wrong = ax.scatter(x_range[~correct_choices], y_values_jittered[~correct_choices],
                                  label='wrong', color='k',alpha=alpha)
     ax.set_yticks([0,1], ['L', 'R'])
-    plt.legend()
-    plt.xlabel("session #", fontsize = 15)
-    plt.ylabel("choice", fontsize = 15)
+    ax.legend()
+    ax.set_xlabel("trial #", fontsize = 25)
+    ax.set_ylabel("choice", fontsize = 25)
+    ax.set_title(f"Choices (Set {j})", fontsize=25)
     #plt.savefig(f'choices_fig{out}session{j}.png')
-    return fig
+    #return fig
     
 
 
 if __name__ == "__main__":
     args = get_args()
-    start = args.start
+    start = args.start # starting number of trials
     end = args.end  # Use the 'end' argument to specify the ending number of trials
-    inc = args.increment
-    num_sess = args.session
-    out = args.outfile
+    inc = args.increment # increment number of trials
+    num_sess = args.session # number of sessions
+    out = args.outfile # name for plot figure
 
     # Set the parameters of the GLM-HMM
     num_states = 3        # number of discrete states
@@ -165,7 +179,8 @@ if __name__ == "__main__":
     N_iters = 1000          # number of fit iterations 
     # set stimulus values negative value correct choice is left [0], positive value correct choice 
     # is right[1]
-    stim_vals = [-1, -0.5, -0.25, -0.125, -0.0625, 0, 0.0625, 0.125, 0.25, 0.5, 1]
+    
+
     # set weights and transition matrix numbers for the HMM model initiated with specific starting
     #  parameters
     gen_weights = np.array([[[6, 1]], [[2, -3]], [[2, 3]]])
@@ -173,25 +188,25 @@ if __name__ == "__main__":
     # initialize an glmhmm with specific parameters
     true_glmhmm = make_standard_hmm(gen_weights, gen_log_trans_mat)
 
-    
-
     for j in range(start, end + 1, inc):
-        
         inpts=generate_inputs(j)
-        
         true_ll, true_latents, true_choices = true_ll_model(j, num_sess)
-        
-        #import ipdb; ipdb.set_trace()
+      
         # inialize new hmm without standard input values
         new_glmhmm = ssm.HMM(num_states, obs_dim, input_dim, observations="input_driven_obs", 
                             observation_kwargs=dict(C=num_categories), transitions="standard")
-        #new_glmhmm.permute(find_permutation(true_latents[0], new_glmhmm.most_likely_states(true_choices[0], input=inpts[0])))    
-        fit_ll = fit_glm_hmm(new_glmhmm)
-
-        plot_weights(new_glmhmm)
-           
-        plot_states(new_glmhmm)
-           
-        plot_choices(j)
         
-        break
+        #new_glmhmm.permute(find_permutation(true_latents[0], new_glmhmm.most_likely_states(true_choices[0], input=inpts[0])))    
+        fit_ll = fit_glm_hmm(new_glmhmm) # fit newly created glm-hmm
+        # create figure an axes with 3 subplots for plotting
+        fig, axes = plt.subplots(nrows=3, figsize=(30, 30), dpi=80, facecolor='w', edgecolor='k')
+        for i in range(3):
+            if i == 0:
+                plot_weights(new_glmhmm, axes[i]) # plot weights
+            if i == 1:
+                plot_states(new_glmhmm, axes[i]) # plot states
+            if i == 2:
+                plot_choices(j, axes[i]) # plot choices
+        plt.tight_layout()
+        plt.savefig(f'wsc_fig{out}session{j}.png')
+        
